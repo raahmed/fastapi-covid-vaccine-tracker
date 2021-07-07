5a573f8aea02f1f8dbcee36a129a17c5f0a432ba
Rabeea Emad Work <raahmed@microsoft.com>
2021-07-06 19:39:52 -0700

    Adding in math to cefficiently compute vaccine type distribution by country

--- pg-lab.py
+++ pg-lab.py
@@ -3,8 +3,38 @@ import psycopg2
 import csv
 
 
-VACCINE_TYPE = {"COM": "Pfizer/BioNTech", "MOD": "Moderna", "CN": "SinoPharm", "SIN": "Coronavac – Sinovac", "SPU": "Sputnik V", "AZ": "AstraZeneca", "UNK": "Unknown" }
-
+#TODO: A more pythonic way to keep a large dictionary in code.
+VACCINE_TYPE = {"COM": "Pfizer/BioNTech", "MOD": "Moderna", "CN": "SinoPharm", "SIN": "Coronavac – Sinovac", "JANSS": "J&J Janssen", "SPU": "Sputnik V", "AZ": "AstraZeneca", "UNK": "Unknown" }
+COUNTRY_CODES = {
+    "BE": "Belgium", "BG": "Bulgaria", 
+    "CZ": "Czechia", 
+    "FR": "France", 
+    "DK": "Denmark", "DE": "Germany",
+    "EE": "Estonia",
+    "IE": "Ireland",
+    "EL": "Greece",
+    "ES": "Spain",
+    "HR": "Croatia",
+    "IT": "Italy",
+    "CY": "Cypress",
+    "LV": "Latvia",
+    "LT": "Lithuania",
+    "LU": "Luxembourg",
+    "HU": "Hungary",
+    "MT": "Malta",
+    "NL": "Netherlands",
+    "AT": "Austria",
+    "PL": "Poland", "PT": "Portugal",
+    "RO": "Romania",
+    "SI": "Slovenia", "SK":"Slovakia",
+    "FI": "Finland",
+    "SE": "Sweden",
+    "IS": "Iceland",
+    "LI": "Leichtenstein",
+    "CH": "Switzerland",
+    "NO": "Norway",
+    "UK": "United Kingdom"
+    }
 def writeConfig(conn_string):
     with open(".conninfo","w") as source:
         source.write(conn_string)
@@ -20,12 +50,6 @@ def connect():
         except:
             print("Database connection error")
             return None
-def populateDevices():
-    conn=connect()
-    cursor=conn.cursor()
-    cursor.execute('INSERT INTO device_list SELECT distinct device_id,location,location_name FROM raw_data')
-    conn.commit()
-    return None
 
 '''
 The headers for the CSV are as follows. 
@@ -38,9 +62,9 @@ def loadData(filename="data.csv"):
     cursor=conn.cursor()
     cursor.execute('CREATE EXTENSION IF NOT EXISTS postgis')
     cursor.execute('DROP TABLE IF EXISTS raw_data')
-    cursor.execute('DROP TABLE IF EXISTS device_list')
+    cursor.execute('DROP TABLE IF EXISTS vaccine_data')
     cursor.execute('CREATE TABLE raw_data(YearWeekISO text,FirstDose bigint, FirstDoseRefused bigint, SecondDose bigint, UnknownDose bigint, NumberDosesReceived bigint, Region text,Population text, ReportingCountry text, TargetGroup text, Vaccine text, Denominator text);')
-    #cursor.execute('CREATE TABLE device_list(device_id bigint,location geography(POINT,4326),location_name text);')
+    cursor.execute('CREATE TABLE vaccine_data(NumberDosesReceived bigint, ReportingCountry text, Vaccine text);')
 
     conn.commit()
     with open(filename,'r') as incoming:
@@ -48,11 +72,18 @@ def loadData(filename="data.csv"):
         conn.commit()
         return "Data loaded successfully"
 
+def populateVaccineData():
+    conn=connect()
+    cursor=conn.cursor()
+    cursor.execute('INSERT INTO vaccine_data SELECT NumberDosesReceived,ReportingCountry,Vaccine FROM raw_data')
+    conn.commit()
+    return None
+
 def getAllData():
     ret=dict()
     conn=connect()
     cursor=conn.cursor()
-    cursor.execute('SELECT device_id,data FROM raw_data')
+    cursor.execute('SELECT * FROM vaccine_list')
     for entry in cursor.fetchall():
         device=entry[0]
         data=entry[1]
@@ -60,21 +91,6 @@ def getAllData():
             ret[device]=[]
         ret[device].append(data)
     return ret
-
-def getNearestDevice(latitude, longitude):
-    conn=connect()
-    cursor=conn.cursor()
-    cursor.execute('SELECT device_id,location_name,ST_DISTANCE(location,ST_SetSRID(ST_MakePoint(%s,%s),4326)) FROM device_list ORDER BY 3 ASC LIMIT 1',(longitude,latitude))
-    data=cursor.fetchone()
-    print("Device number {0} in {1} is closest.".format(data[0],data[1]))
-    return None
-    
-def getDeviceAverage(device):
-    conn=connect()
-    cursor=conn.cursor()
-    cursor.execute("select data -> 'temperature' -> 'units',avg((data -> 'temperature' ->> 'value')::float) from raw_data where device_id = %s group by 1",(device,))
-    data=cursor.fetchone()
-    return data
 
 def runSQL(statement):
     conn=connect()
@@ -90,33 +106,56 @@ def runSQL(statement):
 # I want this to be fed by a FastAPI
 # I want this to have a beautiful interface.
 
-def getTotalVaccineTypeCountsByCountry():
-    # For each country
-    # Summarize the total counts of each vaccine
-    # Map the type and country names to make it human readable
-    return
-
 ## Use FastAPI to read TotalVaccinesByCountry and return data via API.
 # Make it pretty with fancy JS.
+#def countryVaccineDistribution():
 
-def getAverageTemperatures():
-    ret=dict()
+def getCountryVaccinePercentages():
+    vaccine_dose_counts = getCountryVaccineCounts()
+    for country in vaccine_dose_counts.keys():
+        total_doses = vaccine_dose_counts[country]["total_doses"]
+        total_percentage = 0
+        for vaccine_type in vaccine_dose_counts[country].keys():
+            if vaccine_type == "total_doses" or total_doses == 0:
+                pass
+            else:
+                percentage = vaccine_dose_counts[country][vaccine_type] * 100/(1.0 * total_doses)
+                vaccine_dose_counts[country][vaccine_type] = percentage
+                total_percentage += percentage
+        # if total_percentage > 100 or total_percentage < 98:
+        #     print(country, total_percentage)
+    return vaccine_dose_counts
+
+def getCountryVaccineCounts():
+    summary_information = dict()
     conn=connect()
     cursor=conn.cursor()
-    cursor.execute('SELECT device_id,location_name,data FROM raw_data')
+    cursor.execute('SELECT distinct NumberDosesReceived, ReportingCountry, Vaccine FROM vaccine_data')
+    conn.commit()
     for entry in cursor.fetchall():
-        location=entry[1]
-        data=entry[2]['temperature']['value']
-        unit=entry[2]['temperature']['units']
-        if location not in ret:
-            ret[location]=[]
-        ret[location].append(data)
-    for location in ret:
-        data=ret[location]
-        average=sum(data)/len(data)
-        print("{0} had an average temperature of {1}".format(location,average))
-    return None
+        doses, country, vaccine_type=entry
+        if country in COUNTRY_CODES:
+            country = COUNTRY_CODES[country]
+        if vaccine_type in VACCINE_TYPE:
+            vaccine_type = VACCINE_TYPE[vaccine_type]
+        if doses == None:
+            doses = 0
+        if country not in summary_information:
+            summary_information[country] = {vaccine_type: doses, "total_doses": doses}
+        else:
+            if vaccine_type not in summary_information[country]:
+                summary_information[country][vaccine_type] = doses
+                summary_information[country]["total_doses"] += doses
+            else:
+                try:
+                    summary_information[country][vaccine_type] += int(doses)
+                    summary_information[country]["total_doses"] += doses
+
+                except:
+                    print("An error occurred")
+                    print(doses, country, vaccine_type)
+    return summary_information
 
 if __name__ == '__main__':
-    fire.Fire({"writeConfig":writeConfig,"loadData":loadData,"getAllData":getAllData,"getAverageTemperatures":getAverageTemperatures,"populateDevices":populateDevices,"getNearestDevice":getNearestDevice,"getDeviceAverage":getDeviceAverage,"runSQL":runSQL})
+    fire.Fire({"writeConfig":writeConfig,"loadData":loadData,"populateVaccineData": populateVaccineData, "getCountryVaccineCounts":getCountryVaccineCounts, "getCountryVaccinePercentages": getCountryVaccinePercentages, "runSQL":runSQL})
 
